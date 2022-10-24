95eb9712d863eba2c11190f025f77c8c2752d5a6
mc20t <81164509+mc20t@users.noreply.github.com>
2022-10-24 16:57:39 +0300

    2410

---
+++ Shop/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Shop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Content = new Pages.MainPage();
+        }
+
+    }
+}
---
+++ Shop/Pages/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Shop.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ClientPage());
+        }
+
+        private void TagBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new TagPage());
+        }
+
+        private void AdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AdminPage());
+        }
+
+        private void RegionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegionPage());
+        }
+
+        private void ProductBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProductPage());
+        }
+
+        private void CategoryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new CategoryPage());
+        }
+
+        private void ManufacturerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ManufacturerPage());
+        }
+
+        private void UnitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new UnitPage());
+        }
+
+        private void ProductSaleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProductSalePage());
+        }
+
+        private void PurchaseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PurchasePage());
+        }
+
+        private void ClAdmRegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ClAdmRegPage());
+        }
+
+        private void TagOfCl_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new TagOfClientPage());
+        }
+
+        private void ManufProd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ManufacturerProductPage());
+        }
+
+        private void Money_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MoneyPage());
+        }
+    }
+}
